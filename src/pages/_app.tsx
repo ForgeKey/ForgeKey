7d5ccc7433f8cd249b558fe357e9a31bf649bf75
saeta-eth <saetaeth@proton.me>
2025-11-10 22:26:55 -0300

    refactor: remove KeystoreProvider in favor of Zustand

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,12 @@
 import '@/styles/globals.css';
 
 import type { AppProps } from 'next/app';
-import { KeystoreProvider } from '@/contexts/keystore-context';
 import { ZeroizeProvider } from '@/contexts/zeroize-context';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ZeroizeProvider>
-      <KeystoreProvider>
-        <Component {...pageProps} />
-      </KeystoreProvider>
+      <Component {...pageProps} />
     </ZeroizeProvider>
   );
 }
